674579606a672a572342f3cb150fb1b51cebd993
zoyspace <spacemajestic@yahoo.co.jp>
2023-07-04 12:51:40 +0900

    rm @astro/netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify/functions";
+// import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
 	experimental: {
 		assets: true,
 	},
-	output: "server",
-	adapter: netlify(),
+	// output: "server",
+	// adapter: netlify(),
 });
